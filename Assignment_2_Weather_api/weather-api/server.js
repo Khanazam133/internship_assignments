e06c358accc902ba142bd411c10b5038d971e843
Khanazam133 <khanazam133@gmail.com>
2025-06-24 16:31:47 +0530

    Update server.js

--- Assignment_2_Weather_api/weather-api/server.js
+++ Assignment_2_Weather_api/weather-api/server.js
@@ -5,11 +5,11 @@ const weatherRoutes = require('./routes/weatherRoutes');
 const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/weatherDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log("MongoDB connected"))
-  .catch(err => console.error(err));
+// mongoose.connect('mongodb://localhost:27017/weatherDB', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// }).then(() => console.log("MongoDB connected"))
+//   .catch(err => console.error(err));
 
 app.use('/api/weather', weatherRoutes);
 
